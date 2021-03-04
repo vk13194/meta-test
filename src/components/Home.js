0235b9ec5f16461b704d9395e2e4fbf98b8c324d
vk13194 <vijaym13194@gmail.com>
2021-03-04 11:23:24 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Home = () => {
+    return (
+        <div>
+        <Helmet>
+        <meta property="og:title" content="A Step-by-Step Guide to Setting Up Selenium-webdriver with Node.js" data-rh="true"/>
+<meta property="og:description" content="A dummies guide to getting up and running with selenium-webdriver" data-rh="true"/>
+<meta property="og:image" content="https://miro.medium.com/max/812/1*1xhuVp8f2WFUGUByHS8VTg.png" data-rh="true"/>
+        </Helmet>
+    <h3>this is home</h3>
+        </div>
+    )
+}
+
+export default Home
